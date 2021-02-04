00089340e0d34887da9c5904e20d0aa7c9a86761
zsfVishnu-d11 <66246684+zsfVishnu-d11@users.noreply.github.com>
2021-02-04 16:17:09 +0530

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,1 +1,10 @@
-print(2 + 3)
+final_dictionary = {}
+final_dictionary['global'] = 'global_sheet'
+final_dictionary['alias'] = 'alias_sheet'
+final_dictionary['category'] = 'token'
+final_dictionary['version'] = 'version'
+
+print(version)
+
+with open(ROOT_DIR+'/../jsons/' + token + '.json','w') as outfile:
+  json.dump(final_dictionary,outfile,indent = 4)
